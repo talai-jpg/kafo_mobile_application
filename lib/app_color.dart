e75752a59d715d818172e03720f06b84efef7749
rana <ranasultan1188@gmail.com>
2025-10-02 15:18:07 +0300

    adding primary colors

--- lib/app_color.dart
+++ lib/app_color.dart
@@ -1,3 +1,30 @@
+import 'dart:ui';
+
 class AppColor {
+  
+  //grean colors
+  static const Color primary_500 = Color(0xFF1a3e2e);
+  static const Color primary_400 = Color(0xFF2d6a4f);
+  static const Color primary_300 = Color(0xFF429c75);
+  static const Color primary_200 = Color(0xFF429c75);
+  static const Color primary_100 = Color(0xFF68bf98);
+
+
+
+  //Secondary
+  static const Color secondary_500 = Color(0xFF674747);
+  static const Color secondary_400 = Color(0xFF926666);
+  static const Color secondary_300 = Color(0xFFb29090);
+  static const Color secondary_200 = Color(0xFFd1bcbc);
+  static const Color secondary_100 = Color(0xFFefe8e8);
+
+  
+
+
+
+
+
+  //neutral
+  static const Color white = Color(0xffffffff);
   
 }
